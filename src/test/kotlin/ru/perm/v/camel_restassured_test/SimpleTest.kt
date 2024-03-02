fada8d899cad3d3c9dec1e478d136562c8daa6fe
Черепахин Василий <vasi.che@gmail.com>
2024-03-02 14:56:38 +0500

    init proj

---
+++ src/test/kotlin/ru/perm/v/camel_restassured_test/SimpleTest.kt
@@ -0,0 +1,11 @@
+package ru.perm.v.camel_restassured_test
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SimpleTest {
+    @Test
+    fun simple() {
+        assertEquals(1, 1)
+    }
+}
