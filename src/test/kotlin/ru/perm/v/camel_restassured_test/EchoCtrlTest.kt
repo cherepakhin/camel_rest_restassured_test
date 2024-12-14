c0ddb8e59d05559d40549f1a4aad4a76210028f3
Черепахин Василий <vasi.che@gmail.com>
2024-12-14 21:12:56 +0500

    fix unused import warning

--- src/test/kotlin/ru/perm/v/camel_restassured_test/EchoCtrlTest.kt
+++ src/test/kotlin/ru/perm/v/camel_restassured_test/EchoCtrlTest.kt
@@ -9,7 +9,6 @@ import org.hamcrest.core.IsEqual
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import java.lang.String.format
 import kotlin.test.assertEquals
 
 
